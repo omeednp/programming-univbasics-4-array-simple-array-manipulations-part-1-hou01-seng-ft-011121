2020a9269e0c5427362ae0d6c9b9f79d1e07636a
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-04 02:29:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,15 +3,15 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
-  
+  array.unshift(string)
 end
 
 def using_pop(array)
-  
+  array.pop
 end
 
 def pop_with_args(array)
-  
+  array.pop(2)
 end
 
 def using_shift(array)
